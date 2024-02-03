75106ff5d794f34390d546954a3d14ef6b27069a
felixmanus <ghukasyan.programming@gmail.com>
2024-02-03 14:09:04 -0800

    Update prisma schema

--- prisma/migrations/20240204015526_init/migration.sql
+++ prisma/migrations/20240204015526_init/migration.sql
@@ -3,7 +3,9 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
-    "name" TEXT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -20,10 +22,24 @@ CREATE TABLE "Password" (
 CREATE TABLE "Topic" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "parentId" TEXT,
     "userId" TEXT NOT NULL,
 
     CONSTRAINT "Topic_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "topicId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -37,6 +53,22 @@ CREATE TABLE "Session" (
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Verification" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "type" TEXT NOT NULL,
+    "target" TEXT NOT NULL,
+    "secret" TEXT NOT NULL,
+    "algorithm" TEXT NOT NULL,
+    "digits" INTEGER NOT NULL,
+    "period" INTEGER NOT NULL,
+    "charSet" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3),
+
+    CONSTRAINT "Verification_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
@@ -44,7 +76,13 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_email_username_key" ON "User"("email", "username");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
+
+-- CreateIndex
+CREATE INDEX "Article_topicId_idx" ON "Article"("topicId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Session_userId_key" ON "Session"("userId");
@@ -52,11 +90,20 @@ CREATE UNIQUE INDEX "Session_userId_key" ON "Session"("userId");
 -- CreateIndex
 CREATE INDEX "Session_userId_idx" ON "Session"("userId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Verification_target_type_key" ON "Verification"("target", "type");
+
 -- AddForeignKey
 ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Topic" ADD CONSTRAINT "Topic_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Topic"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Topic" ADD CONSTRAINT "Topic_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Article" ADD CONSTRAINT "Article_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "Topic"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,15 +6,57 @@ async function seed() {
 
 	await cleanupDb(prisma);
 
-	await prisma.user.create({
+	const user = await prisma.user.create({
 		data: {
-			email: 'example@gmail.com',
-			name: 'Norayr Ghukasyan',
+			email: 'ghukasyan.programming@gmail.com',
+			verified: true,
+			firstName: 'Norayr',
+			lastName: 'Ghukasyan',
 			username: 'felix_manus',
 			password: {
-				create: createPassword('12345678'),
+				create: createPassword('Test1234'),
 			},
 		},
+	});
+
+	await prisma.topic.createMany({
+		data: [
+			{ title: 'Javascript', userId: user.id },
+			{ title: 'NodeJS', userId: user.id },
+			{ title: 'Typescript', userId: user.id },
+			{ title: 'Network', userId: user.id },
+			{ title: 'SSH', userId: user.id },
+			{ title: 'Remix', userId: user.id },
+			{ title: 'NextJS', userId: user.id },
+			{ title: 'HTML5', userId: user.id },
+			{ title: 'CSS3', userId: user.id },
+			{ title: 'React', userId: user.id },
+			{ title: 'CI/CD', userId: user.id },
+			{ title: 'DB & ORMs', userId: user.id },
+		],
+	});
+
+	const jsTopic = await prisma.topic.findFirst({ where: { title: 'Javascript' } });
+
+	await prisma.topic.createMany({
+		data: [
+			{ title: 'Objects', parentId: jsTopic?.id, userId: user.id },
+			{ title: 'Arrays', parentId: jsTopic?.id, userId: user.id },
+			{ title: 'API', parentId: jsTopic?.id, userId: user.id },
+			{ title: 'FP', parentId: jsTopic?.id, userId: user.id },
+			{ title: 'OOP', parentId: jsTopic?.id, userId: user.id },
+			{ title: 'ES6', parentId: jsTopic?.id, userId: user.id },
+			{ title: 'Design Patterns', parentId: jsTopic?.id, userId: user.id },
+		],
+	});
+
+	const jsObjectsTopic = await prisma.topic.findFirst({ where: { title: 'Objects' } });
+
+	await prisma.article.createMany({
+		data: [
+			{ title: 'How correctly delete values from object', content: 'Some test content', topicId: jsObjectsTopic?.id! },
+			{ title: 'What are objects', content: 'Some test content about objects', topicId: jsObjectsTopic?.id! },
+		],
 	});
 }
 

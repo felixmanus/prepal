279f5a1d201207145034311dde5cf53a54a99d0a
felixmanus <ghukasyan.programming@gmail.com>
2024-02-06 12:04:09 -0800

    Update type definition files

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,1 +1,14 @@
 type allKeys<T> = T extends any ? keyof T : never;
+
+type ActionReturnType<T> =
+	| {
+			status: 'idle' | 'error' | 'success';
+			submission: Submission<T>;
+	  }
+	| {
+			status: 'error' | 'success';
+			message: string;
+			submission?: null;
+	  }
+	| null
+	| undefined;
